b720141fa117ab2b9a2259a3148f88071c659b63
antonokulov <qantus@mail.ru>
2014-10-01 13:11:43 +0400

    i18n

--- messages/ru/main.php
+++ messages/ru/main.php
@@ -1,15 +1,15 @@
 <?php
 
 return array (
-  'Name' => 'Имя',
+  'review' => 'отзыв',
+  'reviews' => 'отзывы',
+  'Reviews' => 'Отзывы',
   'Email' => 'Электронная почта',
+  'Name' => 'Имя',
   'Content' => 'Отзыв',
   'User' => 'Пользователь',
   'Is published' => 'Опубликовано',
   'Created time' => 'Время создания',
   'Updated time' => 'Время изменения',
   'Published time' => 'Время публикации',
-  'Reviews' => 'Отзывы',
-  'reviews' => 'отзывы',
-  'review' => 'отзыв',
 );
