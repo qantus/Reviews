b372cd1d92054d7021f5db2fbf7e0b44409642a5
Falaleev Maxim <max@studio107.ru>
2014-12-22 16:48:29 +0300

    up

--- Forms/ReviewUserForm.php
+++ Forms/ReviewUserForm.php
@@ -17,6 +17,7 @@ namespace Modules\Reviews\Forms;
 use Mindy\Base\Mindy;
 use Mindy\Form\Fields\CharField;
 use Mindy\Form\Fields\EmailField;
+use Mindy\Form\Fields\TextField;
 use Mindy\Form\ModelForm;
 use Modules\Core\Components\ParamsHelper;
 use Modules\Reviews\ReviewsModule;
@@ -42,11 +43,23 @@ class ReviewUserForm extends ModelForm
                 'class' => EmailField::className(),
                 'label' => ReviewsModule::t('Email'),
                 'required' => true,
+                'html' => [
+                    'placeholder' => ReviewsModule::t('Email')
+                ]
             ],
             'name' => [
                 'class' => CharField::className(),
                 'label' => ReviewsModule::t('Name'),
                 'required' => true,
+                'html' => [
+                    'placeholder' => ReviewsModule::t('Name')
+                ]
+            ],
+            'content' => [
+                'class' => TextField::className(),
+                'html' => [
+                    'placeholder' => ReviewsModule::t('Content')
+                ]
             ]
         ];
     }
